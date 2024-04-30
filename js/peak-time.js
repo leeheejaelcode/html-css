4ffec5bc813f1e462a0a0b38d45c0f544d46e1db
heejae <leeheejaelcode@gmail.com>
2024-04-30 15:20:06 +0900

    유저인터렉션 속성

--- js/peak-time.js
+++ js/peak-time.js
@@ -1,8 +1,8 @@
-const button = document.querySelector('.voting-result-button');
-const results = document.querySelectorAll('.voting-result');
+const button = document.querySelector(".voting-result-button");
+const results = document.querySelectorAll(".voting-result");
 
 button.addEventListener("click", () => {
-  results.forEach(result => {
-    result.removeAttribute('hidden');
+  results.forEach((result) => {
+    result.removeAttribute("hidden");
   });
 });
